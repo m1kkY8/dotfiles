d83a20f6e113b1da0a2054f0921f63f933437369
Milan Petrovic <5rovicmilan8@gmail.com>
2024-02-06 13:20:23 +0100

    moved nvim config to new repo

--- .config/nvim/lua/plugins.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {'eandrju/cellular-automaton.nvim'},
-    { 'j-hui/fidget.nvim', opts = {} },
-    {'tpope/vim-fugitive'},
-    {'tpope/vim-rhubarb'},
-    {"folke/neodev.nvim"},
-    {"folke/which-key.nvim"},
-}
